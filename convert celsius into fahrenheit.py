b159f19fc9ea643dbca4db49d72b0b5c0ab6cff1
osneks <124896837+osneks@users.noreply.github.com>
2023-02-14 23:44:42 +0530

    Add files via upload

---
+++ convert celsius into fahrenheit.py
@@ -0,0 +1,31 @@
+x = str(input("Celsius  or Fahrenheit"))
+
+if len(x) == 7  :
+    def far(cel):
+        print((cel * 9/5) + 32)
+
+    c = int(input("Enter the degree : "))
+    far(c)
+else:
+    def cel(far):
+        print ( (far - 32)*(5/9) )
+    f = int(input("Enter the degree : "))
+    cel(f)
+
+
+
+
+        
+
+
+
+
+
+    
+
+
+
+    
+
+
+    
